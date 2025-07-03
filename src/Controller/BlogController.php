8c47092d3bfd9dfb7546840c63e7a0318b12fa1c
Terence Dinguilu <terence.dinguilu@gmail.com>
2025-07-03 17:43:36 +0200

    blog

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class BlogController extends AbstractController
+{
+    #[Route('/', name: 'app_blog')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+        ]);
+    }
+    }
+
+
